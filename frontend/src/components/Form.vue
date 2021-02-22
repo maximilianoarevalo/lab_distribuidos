81de84a8641b771a0796d46557c7947ecd18dcd9
Maxi <maximiliano.arevalo@usach.cl>
2021-02-22 14:29:30 -0300

    Frontend and fix

---
+++ frontend/src/components/Form.vue
@@ -0,0 +1,108 @@
+<template>
+<div class="content">
+  <div class="head">
+    <h1>Predicción de datos</h1>
+    <hr>
+  </div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group" label="Dato:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.original"
+          required
+          placeholder="Ingrese el valor"
+        ></b-form-input>
+      </b-form-group>
+     
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+        <b-button class="a" type="submit" variant="success" @click="showDismissibleAlert=true" >Generar predicción</b-button>
+        </b-col>
+        <b-col>
+        <b-button class="b" type="reset" variant="primary" @click="showDismissibleAlert=false">Solicitar nueva predicción</b-button>
+        </b-col>
+      </b-row>
+    </b-container>    
+    </b-form>
+    <div class="alert">
+    <b-alert v-model="showDismissibleAlert" variant="success" dismissible>
+      <h1>
+        Predicción generada exitosamente
+      </h1>
+      Valor predecido: {{this.predict}} <br/> 
+    </b-alert>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+    data() {
+      return {
+        form: {
+          original: '',
+        },
+        show: true,
+        showDismissibleAlert: false,
+        predict: 0
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        console.log(this.form)
+        let parameters = {
+          original: this.form.original,
+        };
+        //axios.post('http://localhost:8080/app/newPass',parameters)
+        axios.post('http://35.199.69.82:8080/app/newPass',parameters)
+        .then((response) => {
+          this.predict = response.data.predict
+        })
+        .catch((error)=> console.log(error))
+        //alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.original = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      },
+    }
+  }
+</script>
+
+<style>
+#input-group{
+  margin-left:  25%;
+  margin-right: 25%;
+}
+.head{
+  padding: 10px; 
+}
+
+.content{
+  margin-bottom: 15px;
+}
+.alert{
+  margin-top: 5px;
+  margin-left:  50px;
+  margin-right: 50px;
+}
+.a{
+  margin-left:  200px;
+  width: 200px;
+
+}
+.b{
+  margin-right: 200px;
+  width: 200px;
+}
+
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <b-card title="Permiso generado exitosamenteXD">
+    <b-card-text>
+      <!-- Consumo el JSON -->
+      Valor predecido: {{predict}}
+    </b-card-text>
+
+    <b-button class="mt-3" variant="success" href="/">Solicitar nueva predicción</b-button>
+  </b-card>
+</div>
+</template>
+
+
+<script>
+import axios from 'axios'
+  export default {
+    data() {
+      return {
+        dialog: false,
+        predict: ''
+      }
+    },
+    methods:{
+      setInfo: function(Result){
+        this.dialog = false;
+        console.log(Result)
+        this.predict = Result.predict
+      }
+    },
+    mounted(){
+      //axios.get('http://localhost:8080/app/pass')
+      axios.get('http://35.199.69.82:8080/app/pass')
+      .then(response => (this.info = response))
+    }
+  }
+</script>
+
+<style>
+
+</style>
